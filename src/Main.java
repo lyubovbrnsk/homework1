9dcb1b41ae62618e9f6abf8fd95f407eae0a5422
lyubovbrnsk <brnsssskl@gmail.com>
2023-02-20 20:38:11 +0500

    Add Main and README.md

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[] {1, 8, 19, 2, 58});
+        System.out.println(salesManager.max());
+    }
+}
