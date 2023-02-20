def52f7cfc3df61a1364a1a46bd5f236f3b71480
lyubovbrnsk <brnsssskl@gmail.com>
2023-02-21 00:10:45 +0500

    Add min() and average() to SalesManager

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {1, 8, 19, 2, 58});
+        SalesManager salesManager = new SalesManager(new int[] {1, 8, 20, 58});
         System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+        System.out.println(salesManager.average());
     }
 }
